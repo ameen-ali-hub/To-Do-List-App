778e3e2487ab083deff98834479c19bd355b5ac5
ameenali <mqtdyh122@gmail.com>
2023-11-09 11:54:57 -1200

    main

--- lib/backend/Model.dart
+++
@@ -1,8 +0,0 @@
-// ignore_for_file: non_constant_identifier_names, file_names
-
-import 'package:flutter/material.dart';
-// import 'package:hive/hive.dart';
-
-class Model extends ChangeNotifier {
-  String Title = "Good Morning ";
-}
